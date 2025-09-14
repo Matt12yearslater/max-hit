8b9424d545acdb1e5452bdb7c56e97c0de782609
Seebity <seebs.coding@gmail.com>
2025-09-14 09:26:02 -0500

    Renamed ExamplePluginTest to MaxHitPluginTest

--- src/main/java/com/maxhit/utils/utils.java
+++
@@ -1,5 +0,0 @@
-package com.maxhit.utils;
-
-public class utils {
-    public static int booleanToInt(boolean value) { return value ? 1 : 0;}
-}
--- src/test/java/com/maxhit/MaxHitPluginTest.java
+++ src/test/java/com/maxhit/MaxHitPluginTest.java
@@ -3,7 +3,7 @@ package com.maxhit;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MaxHitPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
